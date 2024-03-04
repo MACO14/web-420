c514886fceff295c098387ac97f16c7154e88412
Mackenzie Ortiz <mlubben1@gmail.com>
2024-03-03 20:38:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,32 @@
 // Date: 1/14/2024
 // Description: app.js
 
+import { ComposerAPI } from './routes/Lubben-Ortiz-composer-routes';
+import { PersonAPI } from './routes/Lubben-Ortiz-person-routes';
+import { userAPI } from './routes/Lubben-Ortiz-session-routes';
+import { customerAPI } from './routes/Lubben-Ortiz-node-shopper-routes'
 
 // require statements
 
-const express = require("express");
-const http = require("http");
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
+const express = require('express');
+const http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const mongoose = require('mongoose');
+const composerAPI = require('./routes/Lubben-Ortiz-composer-routes');
+const personAPI = require('./routes/Lubben-Ortiz-person-routes');
+const userAPI = require('./routes/Lubben-Ortiz-session-routes');
+const customerAPI = require('./routes/Lubben-Ortiz-node-shopper-routes');
+
 
 // instance of the express() module
 const app = express();
 
 // app.use statements
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(express.json());
-app.use(express.urlencoded({"extended": true}));
+app.use(express.urlencoded({'extended': true}));
 
 // swagger configurations
 
@@ -36,9 +46,13 @@ const options = {
 
 const openapiSpecification = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+app.use('/api', composerAPI);
+app.use('/api', personAPI);
+app.use('/api', userAPI);
+app.use('/api', customerAPI);
 
 // createServer
-http.createServer(app).listen(app.get("port"), function () {
+http.createServer(app).listen(app.get('port'), function () {
     console.log("Application started and listening on port" + app.get("port"));
 })
 
